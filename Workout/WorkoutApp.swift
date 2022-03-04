d2c9c20d1648f0e66bafae7307707558def5763b
Alex Schlageter <realex688@gmail.com>
2022-03-04 08:20:53 +0100

    Initial Commit

---
+++ Workout/WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp.swift
+//  Workout
+//
+//  Created by schlageter alex on 04/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
