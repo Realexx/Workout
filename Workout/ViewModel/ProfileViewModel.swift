cc87b54a20a006d55a0148732b177e8b35057e51
okarim <ounis.karim008@gmail.com>
2022-03-25 10:35:23 +0100

    debug user

--- Workout/ViewModel/ProfileViewModel.swift
+++ Workout/ViewModel/ProfileViewModel.swift
@@ -17,5 +17,7 @@ class ProfileViewModel : ObservableObject{
     func addUser(firstName: String, lastName: String,sexe: Sexe, birth: Date){
         let newUser = User(firstName: firstName, lastName: lastName, sexe: sexe, birth: birth)
         users.append(newUser)
+        
+        print("CREATION USER : ",newUser)
     }
 }
