2cf744de7cad9dde4f6e3562721f809f0b08a6b6
okarim <ounis.karim008@gmail.com>
2022-03-25 10:28:44 +0100

    Integration de la distance + de pins

--- Workout/ViewModel/MapViewModel.swift
+++ Workout/ViewModel/MapViewModel.swift
@@ -13,22 +13,27 @@ class MapViewModel: NSObject,ObservableObject,CLLocationManagerDelegate{
     
     @Published var maps: [MapModel] = []
     
-    var distance : Int = 0
+    var distance : Double = 0
     
     override init(){
         super.init()
         
         print("Création du map view model")
-        
-        
+        setMapTest()
+    }
+    
+    func setMapTest(){
+        maps.append(MapModel())
     }
     
     func startRecord(){
         print("Test StartRecord")
-        let map = MapModel()
-        map.startRecord()
-        maps.append(map)
-        distance = map.dist
+        // let map = Map()
+        let map = maps.last
+        map?.startRecord()
+       // maps.append(map)
+        distance = map!.dist
+        print("debug startrecord",distance)
     }
     
     func stopRecord(){
@@ -36,6 +41,45 @@ class MapViewModel: NSObject,ObservableObject,CLLocationManagerDelegate{
         let map = maps.last
         map?.stopRecord()
     }
+    
+    func pauseRecord(){
+        let map = maps.last
+        map!.pauseRecord()
+    }
+    /*
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
+        let map = maps.last
+        
+        let p1 = MKPlacemark(coordinate: map?.annotationsItems.first!.coordinate)
+
+      // Boston
+        let p2 = MKPlacemark(coordinate: map?.annotationsItems.last!.coordinate)
+
+        
+        let request = MKDirections.Request()
+         request.source = MKMapItem(placemark: p1)
+         request.destination = MKMapItem(placemark: p2)
+         request.transportType = .automobile
+
+         let directions = MKDirections(request: request)
+         directions.calculate { response, error in
+           guard let route = response?.routes.first else { return }
+           mapView.addAnnotations([p1, p2])
+           mapView.addOverlay(route.polyline)
+           mapView.setVisibleMapRect(
+             route.polyline.boundingMapRect,
+             edgePadding: UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20),
+             animated: true)
+         }
+        
+        return mapView
+      }
+     */
+    
+    
     
     
    
